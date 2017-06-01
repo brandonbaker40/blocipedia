7570ac4ddd036bd15217e1d3a643c722e043024c
Brandon Baker <brandonbaker40@icloud.com>
2017-06-01 09:08:55 -0400

    tests sign in for all users  on user controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,42 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-#   let(:new_standard_user_attributes) do
-#    {
-#        email: "standard@bloc.io",
-#        password: "blochead",
-#        role: 0
-#    }
-#  end
-#
-#  let(:new_premium_user_attributes) do
-#   {
-#       email: "premium@bloc.io",
-#       password: "blochead",
-#       role: 1
-#   }
-# end
-#
-# let(:new_admin_user_attributes) do
-#  {
-#      email: "admin@bloc.io",
-#      password: "blochead",
-#      role: 2
-#  }
-# end
-#
-# describe "GET #index" do
-#   it "returns http success" do
-#     sign_in @user
-#     get :index
-#     expect(response).to have_http_status(:success)
-#   end
-#
-#      it "instantiates a new user" do
-#        get :new
-#        expect(assigns(:user)).to_not be_nil
-#      end
-#    end
-#
+  let(:new_standard_user_attributes) do
+   {
+       email: "standard@bloc.io",
+       password: "blochead",
+       role: 0
+   }
+ end
+
+ let(:new_premium_user_attributes) do
+  {
+      email: "premium@bloc.io",
+      password: "blochead",
+      role: 1
+  }
+end
+
+let(:new_admin_user_attributes) do
+ {
+     email: "admin@bloc.io",
+     password: "blochead",
+     role: 2
+ }
+end
+
+
+###### ADMIN USER ########
+  describe "admin user sign in" do
+    login_admin_user
+
+    it "should have a current_user" do
+      expect(subject.current_user).to_not eq(nil)
+    end
+
+    it "should get index" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+
+###### PREMIUM USER ########
+  describe "premium user sign in" do
+    login_premium_user
+
+    it "should have a current_user" do
+      expect(subject.current_user).to_not eq(nil)
+    end
+
+    it "should get index" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+
+###### STANDARD USER ########
+  describe "standard user sign in" do
+    login_premium_user
+
+    it "should have a current_user" do
+      expect(subject.current_user).to_not eq(nil)
+    end
+
+    it "should get index" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
 
 end
---
+++ spec/support/controller_macros.rb
@@ -0,0 +1,25 @@
+module ControllerMacros
+  def login_admin_user
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      admin = User.create(email: 'blocadmin@bloc.io', password: 'honeybunchesofoats1234', role: 2)
+      sign_in admin
+    end
+  end
+
+  def login_premium_user
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:premium]
+      premium = User.create(email: 'blocpremium@bloc.io', password: 'luckycharms1234', role: 1)
+      sign_in premium
+    end
+  end
+
+  def login_standard_user
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:standard]
+      standard = User.create(email: 'blocstandard@bloc.io', password: 'frostedflakes1234', role: 0)
+      sign_in standard
+    end
+  end
+end
