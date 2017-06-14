cf71f28f81d45a9435368569ab921f022e806948
Brandon Baker <brandonbaker40@icloud.com>
2017-06-14 00:24:14 -0400

    updates seed file to prevent standard users from having private wikis

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,47 @@
 
-# Create Users
-15.times do
+# Create Standard Users
+5.times do
    User.create!(
    email:    Faker::Internet.free_email,
    password: Faker::Internet.password(8),
-   role: Faker::Number.between(0, 2)
+   role: 0
    )
  end
- users = User.all
+ #users = User.all
 
- 50.times do
+
+# Create Premium and Admin Users
+ 10.times do
+    User.create!(
+    email:    Faker::Internet.free_email,
+    password: Faker::Internet.password(8),
+    role: Faker::Number.between(1, 2)
+    )
+  end
+
+
+ #Create wikis by standard users
+ 20.times do
+   post = Wiki.create!(
+
+     title: Faker::StarWars.character,
+     body: Faker::StarWars.quote,
+     private: false, #can't be true because Standard users can't have private wikis
+     user_id: Faker::Number.between(1, 5)
+   )
+ end
+ #wikis = Wiki.all
+
+ #Create wikis by premium and admin users
+ 30.times do
    post = Wiki.create!(
 
      title: Faker::StarWars.character,
      body: Faker::StarWars.quote,
      private: Faker::Boolean.boolean,
-     user_id: Faker::Number.between(1, User.count)
+     user_id: Faker::Number.between(6, User.count)
    )
  end
- wikis = Wiki.all
 
  puts "Seed finished"
  puts "#{User.count} users created"
